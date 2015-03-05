bf724b5fe9921bf407da7c67605ce008226ee531
Andy73 <fok.ondra@seznam.cz>
2015-03-05 17:18:26 +0100

    WIP
    WIP  on the ParticleEffect lib

--- ParticleEffect.lua
+++ ParticleEffect.lua
@@ -1,4 +1,4 @@
-function deepcopy(orig)
+local function deepcopy(orig)
     local orig_type = type(orig)
     local copy
     if orig_type == 'table' then
@@ -38,21 +38,33 @@ particle.New=function(s,p,v)
 	end
 end
 
-lib.New=function()
+--static
+lib.Shape={}
+lib.Shape.Rectangular='ParticleEffect.Shape.Rectangular'
+lib.Shape.Point='ParticleEffect.Shape.Point'
+
+--methods
+lib.New=function(s,p,sz,st,wt)
 	local n={} --instance
+
+	n.Shape=s or lib.Shape.Rectangular
+	n.Position=p or {x=0;y=0;}
+	n.Size=sz or {w=0;h=0;}
+	n.StepTime=st or 1
+	n.WaitTime=wt or 1
 	
 	local particles={}
 
 	n.Render=function()
 		for i,p in ipairs(particles) do
-			term.setCursorPos(unpack(p.Position))
+			term.setCursorPos(n.Position.x+p.Position.x,n.Position.y+p.Position.y)
 			if p.Sprite[2]>0 then term.setTextColor(p.Sprite[2]) end
 			if p.Sprite[3]>0 then term.setBackgroundColor(p.Sprite[3]) end
 			term.write(p.Sprite[1])
 		end
 	end
 
-	n.Emmit=function(n,fn,...) --number of particles, noise function, masks (particle to derive values from)
+	n.EmmitRaw=function(n,fn,...) --number of particles, noise function, masks (particle to derive values from)
 		local masks={...}
 
 		local function newParticle()
@@ -70,7 +82,42 @@ lib.New=function()
 		end
 	end
 
-	n.Frame=function()
-		
+	n.Emmit=function(default,velocity,amount,m,modifier) --default particle, velocity of particles, amount of particles, number of masks to use, modifier
+		--generate masks
+		local masks={}
+
+		local function generateMask()
+			local mask=deepcopy(default)
+			
+			for k,v in pairs(mask) do
+				if type(v)=='table' then
+					for kk,vv in pairs(v) do
+						if type(vv)=='number' then
+							mask[k][kk]=vv*math.random((modifier[k][kk].min or modifier.min),(modifier[k][kk].max or modifier.max))
+						end
+					end
+				end
+			end
+
+			table.insert(masks,mask)
+		end
+
+		for i=1,m do
+			generateMask()
+		end
+
+		--emmit
+		n.EmmitRaw(amount,nil,unpack(masks)) --TODO not nil
 	end
+
+	n.Frame=function()
+		for k,v in pairs(particles) do
+			v.Frame()
+		end
+	end
+
+	return n
 end
+
+_G.ParticleEffect=lib
+_G.Particle=particle
--- main.lua
+++ main.lua
@@ -1,10 +1,15 @@
 local function load( path )
-	path=shell.resolve'./'.."/"..path
+	path='/'..shell.resolve'./'.."/"..path
 	local f=fs.open(path,'r')
 	print('Loading '..path)
 	local fn,err=loadstring(f.readAll(),'lib @'..path)
+	f.close()
 	if fn then return fn() end
 	error(err,0)
 end
 
 load'ParticleEffect.lua'
+
+local MyParticleEffect=ParticleEffect.New()
+MyParticleEffect.Frame()
+MyParticleEffect.Render()
