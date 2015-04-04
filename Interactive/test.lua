25bf231c58a9e39236e2129f0fd4e5ee25dfc18d
Andy73 <fok.ondra@seznam.cz>
2015-04-04 20:22:06 +0200

    WIP interactive

--- Interactive/test.lua
+++ Interactive/test.lua
@@ -3,12 +3,32 @@
 
 --print(textutils.serialize(err))
 
-for i=1,10 do
+sleep(1)
+
+local level={
+	{1,1,1,1,},
+	{1,1,1,1,},
+	{1,1,1,1,},
+	{1,1,1,1,},
+}
+
+local blocks={
+	[0]="0",
+	[1]="155",
+}
+
+for y,row in ipairs(level) do
+	for x,block in ipairs(row) do
+		commands.execAsync("summon FallingSand ~"..y.." ~2 ~"..(x+5).." {TileID:"..block..",Time:1,Motion:[0.0,0.5,0.0]}")
+	end
+end
+
+--[[for i=1,10 do
 	for ii=1,10 do
 		commands.execAsync("summon FallingSand ~"..i.." ~2 ~"..(ii+5).." {TileID:155,Time:1,Motion:[0.0,0.5,0.0]}")	
 	end
 	sleep(0)
-end
+end]]
 --,BlockID:155,DropItem:155
 
 --summon FallingSand x y z {TileID:5,Time:2,Motion:[0.0,0.0,0.5]}
