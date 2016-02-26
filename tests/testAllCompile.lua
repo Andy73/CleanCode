748da67613b8d73074e1c031698990954aa93076
Andrew Kvapil <viluon@espiv.net>
2016-02-26 15:45:57 +0100

    Improve All-Compile Test
    Add support for compiling serialized tables.

--- tests/testAllCompile.lua
+++ tests/testAllCompile.lua
@@ -3,9 +3,28 @@ local files = {
 	"credits.lua";
 }
 
+local tables = {
+	"language.tbl";
+}
+
 print( "Test:\n ALL: Compile" )
 
 for k, path in pairs( files ) do
-	print( "  Compiling " .. path .. "..." )
+	print( "  Compiling file " .. path .. "..." )
+
 	loadfile( "../" .. path )
+end
+
+for k, path in pairs( tables ) do
+	print( "  Compiling table " .. path .. "..." )
+	
+	local f = io.open( "../" .. path )
+	local content = f:read( "*a" )
+	f:close()
+
+	local ok, err = loadstring( "return " .. content )
+
+	if not ok then
+		error( err )
+	end
 end
